2b118a93e62ce023324efddc1a44c38a75fa54e3
Thiago Henrique <thenrique2012@gmail.com>
2020-03-26 11:15:50 -0300

    Pagina Profile Ok

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.svg';
+import { Container, LogoutButton, Header, Incidents, Incident } from './styles';
+
+export default function Profile() {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg} alt="Be the Hero" />
+        <p>Bem vinda, Sociedade Vencer</p>
+
+        <Link to="/incidents" style={{ marginLeft: 'auto' }}>
+          <Button type="button">Cadastrar novo caso</Button>
+        </Link>
+
+        <LogoutButton>
+          <FiPower size={22} color="#e02041" />
+        </LogoutButton>
+      </Header>
+
+      <h1> Casos cadastrados </h1>
+
+      <Incidents>
+        <Incident>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Lorem ipslum dolor</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </Incident>
+
+        <Incident>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Lorem ipslum dolor</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </Incident>
+
+        <Incident>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Lorem ipslum dolor</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </Incident>
+
+        <Incident>
+          <strong>CASO:</strong>
+          <p>Caso teste</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Lorem ipslum dolor</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </Incident>
+      </Incidents>
+    </Container>
+  );
+}
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Login} />
       <Route path="/register" exact component={Register} />
+      <Route path="/profile" exact component={Profile} />
     </Switch>
   </BrowserRouter>
 );
